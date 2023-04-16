930dfff56e17b2d925bbd1faafb40d3e3452e9ac
Marc Scholten <marc@digitallyinduced.com>
2023-04-16 12:32:51 +0200

    Added tasks and tags data structures

--- Application/Migration/1681641158.sql
+++ Application/Migration/1681641158.sql
@@ -1,4 +1,3 @@
--- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
 CREATE TABLE tasks (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     description TEXT NOT NULL
